1a9e644ef51fa82e325d7fb20ede108a0afd31b5
osamaafify667 <osamaafify667@gmail.com>
2026-07-09 13:34:11 +0300

    Add files via upload

---
+++ js/policies lang.js
@@ -0,0 +1,27 @@
+const translations = {
+
+    ar:{
+
+    },
+
+    en:{
+
+    }
+
+};
+
+const translations = {
+
+    ar:{
+
+        heroTitle:" سياسة الخصوصية وسرية المعلومات"
+
+    },
+
+    en:{
+
+        heroTitle:"Privacy and Information Confidentiality Policy"
+
+    }
+
+};
