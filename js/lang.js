4d0c0e38d0c79e8c0e943495dbb0398d51990fa2
osamaafify667 <osamaafify667@gmail.com>
2026-07-09 13:48:46 +0300

    Add files via upload

---
+++ js/lang.js
@@ -0,0 +1,67 @@
+const translations = {
+
+    ar:{
+
+    heroTitle:"ننقل مشروعك إلى مستوى جديد.",
+
+    heroText:"نبني استراتيجيات تسويقية للشركات والمتاجر الإلكترونية والأكاديميات",
+
+    startNow:"ابدأ الآن",
+
+    portfolio:"أعمالنا",
+
+    contact:"تواصل معنا",
+
+    servicesTitle: "خدماتنا",
+
+service1: "بناء استراتيجيات تسويقية",
+
+service2: "إدارة وإنشاء الحملات الإعلانية",
+
+service3: "إنشاء فيديوهات موشن جرافيك",
+
+service4: "إنشاء تصاميم احترافية",
+
+ctaTitle:"جاهز تنقل مشروعك لمستوى احترافي؟",
+
+ctaButton:"راسلنا الآن عبر واتساب",
+
+ctaTitle: "جاهز تنقل مشروعك لمستوى احترافي؟",
+
+ctaButton: "راسلنا الآن عبر واتساب",
+
+    marketingAgency:"MARKETING AGENCY"
+
+},
+
+    en:{
+
+    heroTitle:"Take Your Business To The Next Level.",
+
+    heroText:"We Build Marketing Strategies For Companies, E-commerce Stores & Academies.",
+
+    startNow:"Start Now",
+
+    portfolio:"Portfolio",
+
+    contact:"Contact Us",
+
+    servicesTitle: "Our Services",
+
+service1: "Marketing Strategy",
+
+service2: "Ads Campaign Management",
+
+service3: "Motion Graphics ",
+
+service4: " Graphic Design",
+
+ctaTitle: "Market Your Business Like A Pro.",
+
+ctaButton: "Get Started on WhatsApp",
+
+    marketingAgency:"MARKETING AGENCY"
+
+}
+
+};
